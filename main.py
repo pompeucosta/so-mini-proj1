494db703bbf81ec390ff622a4250906b5d126c64
PC <pompeu@ua.pt>
2024-04-03 14:00:51 +0100

    delete old files

--- main.py
+++
@@ -1,6 +0,0 @@
-from InventorySimulation import InventorySimulation
-
-inv_sim = InventorySimulation(20,80)
-inv_sim.simulate()
-
-print("full_cost: %.2f\norder_cost: %.2f\nhold_cost: %.2f\nshort_cost: %.2f" % (inv_sim.full_costs,inv_sim.order_cost,inv_sim.hold_costs,inv_sim.short_costs)) 
