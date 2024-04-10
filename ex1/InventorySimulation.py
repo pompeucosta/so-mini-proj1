7b5d08bcde77325f1ab9fc449750833b08cff01d
PC <pompeu@ua.pt>
2024-04-10 15:18:46 +0100

    ex1.2

--- ex1/InventorySimulation.py
+++ ex1/InventorySimulation.py
@@ -4,12 +4,16 @@ import random
 from ArrivalEvent import Arrival
 
 class InventorySimulation(Simulation):
-    def __init__(self,small_s: int,big_s: int,inv_lvl: int = 60,months: int = 120) -> None:
+    def __init__(self,small_s: int,big_s: int,inv_initial_lvl: int = 60,months: int = 120) -> None:
         super().__init__()
         self._small_s = small_s
         self._big_s = big_s
-        self._inv_lvl = inv_lvl
         self._months = months
+
+        #setup inventory
+        self._inv = []
+        self._inv_amount = 0
+        self._add_items_to_inventory(inv_initial_lvl)
 
         #statistics
         #costs
@@ -20,8 +24,11 @@ class InventorySimulation(Simulation):
         self._ipos = 0.
         self._ineg = 0.
 
+        #others
         self._num_express_orders = 0
         self._backlog_time = 0.
+        self._num_spoiled_items = 0
+
 
         #initialize events
         super().events.append(Event("eval",0))
@@ -61,42 +68,71 @@ class InventorySimulation(Simulation):
     @property
     def number_of_express_orders(self):
         return self._num_express_orders
+    
+    @property
+    def number_of_spoiled_items_discarded(self):
+        return self._num_spoiled_items
 
     def _order_arrival(self):
         elem = self._remove_current_event_from_list()
-        self._inv_lvl += elem.amount
+        amount = elem.amount
+        if self._inv_amount >= 0:
+            self._add_items_to_inventory(amount)
+        else:
+            if self._inv_amount + amount > 0:
+                added = self._inv_amount + amount
+                #if backlog is fullfilled then add the remaining items to inventory
+                #ex:
+                #inv_amount = -5
+                #amount = 6
+                #added = -5 + 6 = 1
+                #so backlog is fullfilled, then add 1 item to inventory
+                self._inv_amount = 0
+                self._add_items_to_inventory(added)
+            else:
+                #backlog is not totaly fullfilled
+                self._inv_amount += amount
 
     def _demand(self):
         self._remove_current_event_from_list()
         amount = random.choices([1,2,3,4],[1/6,1/3,1/3,1/6])[0]
-        self._inv_lvl -= amount
+        self._remove_spoiled_items()
+        if self._inv_amount > 0:
+            if self._inv_amount < amount:
+                leftovers = amount - self._inv_amount
+                self._remove_inventory_amount(self._inv_amount)
+                self._inv_amount -= leftovers
+            else:
+                self._remove_inventory_amount(amount)
+        else:
+            self._inv_amount -= amount
+
         super().events.append(Event("demand",super().sim_time + random.expovariate(10)))
 
     def _evaluate(self):
         self._remove_current_event_from_list()
 
-        if self._inv_lvl < 0:
+        if self._inv_amount < 0:
             #express order
             self._num_express_orders += 1
-            amount = self._big_s - self._inv_lvl
+            amount = self._big_s - self._inv_amount
             self._express_costs += 48 + 4 * amount
             super().events.append(Arrival(amount,super().sim_time + random.uniform(0.25,0.5)))
-            pass
-        elif self._inv_lvl < self._small_s:
-            amount = self._big_s - self._inv_lvl
+        elif self._inv_amount < self._small_s:
+            amount = self._big_s - self._inv_amount
             self._order_costs += 32 + 3 * amount
             super().events.append(Arrival(amount,super().sim_time + random.uniform(0.5,1)))
 
         super().events.append(Event("eval",super().sim_time + 1))
 
     def _update_statistics(self):
-        if self._inv_lvl == 0:
+        if self._inv_amount == 0:
             return
         
-        if self._inv_lvl > 0:
-            self._ipos += self._inv_lvl * (super().sim_time - self._time_last_event)
+        if self._inv_amount > 0:
+            self._ipos += self._inv_amount * (super().sim_time - self._time_last_event)
         else:
-            self._ineg += (-self._inv_lvl) * (super().sim_time - self._time_last_event)
+            self._ineg += (-self._inv_amount) * (super().sim_time - self._time_last_event)
             self._backlog_time += super().sim_time - self._time_last_event
 
     def _timing(self):
@@ -106,6 +142,24 @@ class InventorySimulation(Simulation):
     def _remove_current_event_from_list(self):
         return super().events.pop(super().current_event_pos)
     
+    def _add_items_to_inventory(self,amount: int):
+        self._inv_amount += amount
+        for _ in range(0,amount):
+            self._inv.append(super().sim_time + random.uniform(1.5,2.5))
+
+    def _remove_inventory_amount(self,amount: int):
+        for _ in range(0,amount):
+            self._remove_item()
+
+    def _remove_spoiled_items(self):
+        while len(self._inv) > 0 and self._inv[0] <= super().sim_time:
+            self._remove_item()
+            self._num_spoiled_items += 1
+
+    def _remove_item(self):
+        self._inv_amount -= 1
+        self._inv.pop(0)
+
     def simulate(self):
         while(super().current_event.type != "end"):
             self._timing()
--- ex1/main.py
+++ ex1/main.py
@@ -3,7 +3,7 @@ from InventorySimulation import InventorySimulation
 def main():
     policies = [(20,40),(20,60),(20,80),(20,100),(40,60),(40,80),(40,100),(60,80),(60,100)]
 
-    print("%9s | %10s | %10s | %10s | %10s | %12s | %12s | %15s" % ("policy","full_cost","order_cost","hold_cost","short_cost","express_cost","backlog_time","express_orders"))
+    print("%9s | %10s | %10s | %10s | %10s | %12s | %12s | %15s | %13s" % ("policy","full_cost","order_cost","hold_cost","short_cost","express_cost","backlog_time","express_orders","spoiled_items"))
     for small_s,big_s in policies:
         inv_sim = InventorySimulation(small_s,big_s)
         inv_sim.simulate()
@@ -11,7 +11,7 @@ def main():
 
 def print_sim_results(inv_sim: InventorySimulation):
     policy = inv_sim.policy
-    print("(%3d,%3d) | %10.2f | %10.2f | %10.2f | %10.2f | %12.2f | %12.2f | %15d" % (policy[0],policy[1],inv_sim.full_costs,inv_sim.order_cost,inv_sim.hold_costs,inv_sim.short_costs,inv_sim.express_costs,inv_sim.backlog_time,inv_sim.number_of_express_orders))
+    print("(%3d,%3d) | %10.2f | %10.2f | %10.2f | %10.2f | %12.2f | %12.2f | %15d | %13d" % (policy[0],policy[1],inv_sim.full_costs,inv_sim.order_cost,inv_sim.hold_costs,inv_sim.short_costs,inv_sim.express_costs,inv_sim.backlog_time,inv_sim.number_of_express_orders,inv_sim.number_of_spoiled_items_discarded))
 
 if __name__ == "__main__":
     main()
