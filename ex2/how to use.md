b18b7e0a16d79029060a12f3b8bcd1d901c7a4ee
PC <pompeu@ua.pt>
2024-05-13 12:18:46 +0100

    update ex2 to read values from cli

---
+++ ex2/how to use.md
@@ -0,0 +1,21 @@
+# Initial values specified through a file
+```bash
+python main.py <file path>
+```
+
+The file must have 7 float values in a line, separated by commas *,*
+
+For example:
+
+5,3,2.5,0.6,1,0.1,120
+
+where each number corresponds to:
+
+s0,i0,r0,beta,k,delta t, t final
+
+The program allows the execution of multiple simulations with different values because it reads the values line by line. This means that, for example, if the file has 4 lines of values then 4 different simulations will be executed.
+
+# Initial values specified through the command line
+```bash
+python main.py s0 i0 r0 beta k delta_t t_final
+```
--- ex2/main.py
+++ ex2/main.py
@@ -1,20 +1,12 @@
 import matplotlib.pyplot as plt
 from SIR import SIR
 from RungeKuttaSIR import RungeKuttaSIR
+from sys import argv
 
-def main():
-    # TODO: ler valores da linha de comandos
-    # valores completamente random
-    # TODO: ver valores bons
-    s0 = 5
-    i0 = 5
-    r0 = 5
-    beta = 5
-    k = 5
-    delta_t = 1
-    t_final = 5
-
-    sir = RungeKuttaSIR(s0,i0,r0,beta,k,delta_t,t_final)
+def simulate(s0,i0,r0,beta,k,dt,tf,output_plt_file):
+    # uncomment the line below to use the Forward Euler Method instead of the Runge Kutta and comment the RungKuttaSIR line
+    # sir = SIR(s0,i0,r0,beta,k,delta_t,t_final)
+    sir = RungeKuttaSIR(s0,i0,r0,beta,k,dt,tf)
     sir.simulate()
 
     susceptible = sir.susceptible
@@ -29,7 +21,39 @@ def main():
     plt.ylabel("Population")
     plt.title("SIR")
     plt.legend()
-    plt.savefig("sir.png")
+    plt.savefig(output_plt_file)
+
+def generate_output_plt_name(i):
+    return "sir" + str(i) + ".png"
+
+def main():
+    file = ""
+    s0 = i0 = r0 = beta = k = delta_t = t_final = 0.
+    if len(argv) == 2:
+        file = argv[1]
+    elif len(argv) == 8:
+        s0 = float(argv[1])
+        i0 = float(argv[2])
+        r0 = float(argv[3])
+        beta = float(argv[4])
+        k = float(argv[5])
+        delta_t = float(argv[6])
+        t_final = float(argv[7])
+    else:
+        print("Invalid number of arguments")
+        exit(1)
+
+    if file != "":
+        with open(file,"rt") as input:
+            for i,line in enumerate(input):
+                values = line.split(',')
+                if len(values) != 7:
+                    print("Invalid number of arguments in line %d" % (i + 1))
+                    exit(2)
+
+                simulate(float(values[0]),float(values[1]),float(values[2]),float(values[3]),float(values[4]),float(values[5]),float(values[6]),generate_output_plt_name(i))
+    else:
+        simulate(s0,i0,r0,beta,k,delta_t,t_final,generate_output_plt_name(""))
 
 if __name__ == "__main__":
     main()
